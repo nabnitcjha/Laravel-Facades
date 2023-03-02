fce1f7c41d4f6c37e8a70819352c6fc691e7f96e
Nabnit Jha <nabnitcjha@gmil.com>
2023-03-03 01:04:52 +0545

    Improve code

--- app/PostCard.php
+++ app/PostCard.php
@@ -2,15 +2,22 @@
 
 namespace App;
 
-class PostCard
+use Illuminate\Support\Facades\Facade;
+
+class PostCard extends Facade
 {
 
-    protected static function resolveFacade($name)
+    // protected static function resolveFacade($name)
+    // {
+    //     return app()[$name];
+    // }
+    // public static function __callStatic($method, $arguments)
+    // {
+    //     return  (self::resolveFacade('PostCard'))->$method(...$arguments);
+    // }
+
+    protected static function getFacadeAccessor()
     {
-        return app()[$name];
-    }
-    public static function __callStatic($method, $arguments)
-    {
-        return  (self::resolveFacade('PostCard'))->$method(...$arguments);
+        return 'PostCard';
     }
 }
